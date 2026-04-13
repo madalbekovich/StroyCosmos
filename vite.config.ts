2704ef9c054badac191491faecf7566ce8d9f697
madalbekovich <talasbekalgaziev759@gmail.com>
2026-04-14 02:54:47 +0600

    feat: add image for directions

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { '@': path.resolve(__dirname, './src') }
+  }
   },
 });
